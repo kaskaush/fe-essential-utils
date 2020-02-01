388f6ef9c949b9fa806b017094b3430aaac1837d
Kaushik Kasibhatla <kaushik.kasibhatla@gmail.com>
2020-02-01 21:47:12 +0530

    added safety check

--- src/accessibility-utils/index.js
+++ src/accessibility-utils/index.js
@@ -7,16 +7,18 @@
  * @param {object} event events
  */
 const focusTrap = (rootElement, event) => {
-    if (event.keyCode === 9) {
-        let focusableEls = rootElement.querySelectorAll(
-            'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]'
-        );
-        if (focusableEls[focusableEls.length - 1] === event.target) {
-            event.preventDefault();
-            focusableEls[0].focus();
-        } else if (event.shiftKey && focusableEls[0] === event.target) {
-            event.preventDefault();
-            focusableEls[focusableEls.length - 1].focus();
+    if (rootElement) {
+        if (event.keyCode === 9) {
+            let focusableEls = rootElement.querySelectorAll(
+                'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]'
+            );
+            if (focusableEls[focusableEls.length - 1] === event.target) {
+                event.preventDefault();
+                focusableEls[0].focus();
+            } else if (event.shiftKey && focusableEls[0] === event.target) {
+                event.preventDefault();
+                focusableEls[focusableEls.length - 1].focus();
+            }
         }
     }
 };
